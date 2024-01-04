e2929b7046b22e4f9a083023fbf117b1a636ff0c
kokoko <r202203448yi@jindai.jp>
2024-01-04 10:55:17 +0900

    using quick head_recursion

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 // 2の6乗を計算する。
 #include <stdio.h>
-int f1(int roop, int result);
-int f1(int roop, int result) {
+int f1(int roop, int result, int tei);
+int jijou(int n);
+
+int f1(int roop, int result, int tei) {
   if (roop < 1) {
     return result;
+  } else if (roop % 2 == 1) {
+    return f1(roop - 1, tei * result, tei);
   } else {
-    return f1(roop - 1, 2 * result);
+    return f1(roop / 2, result, jijou(tei));
   }
 }
+
+int jijou(int n) { return n * n; }
+
 int main(void) {
   int roop = 6;
   int result = 1;
-  printf("%d\n", f1(roop, result));
+  int tei = 2;
+  printf("%d\n", f1(roop, result, tei));
   return 0;
 }
