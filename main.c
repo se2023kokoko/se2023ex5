e4134d6e918870c0d6d800a5c411a638fc9a8883
kokoko <r202203448yi@jindai.jp>
2024-01-04 10:45:50 +0900

    using tail_recursion

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 // 2の6乗を計算する。
 #include <stdio.h>
-
+int f1(int roop);
+int f1(int roop) {
+  if (roop < 1) {
+    return 1;
+  } else {
+    return 2 * f1(roop - 1);
+  }
+}
 int main(void) {
-  printf("%d\n", 2 * 2 * 2 * 2 * 2 * 2);
+  int roop = 6;
+  printf("%d\n", f1(roop));
   return 0;
 }
